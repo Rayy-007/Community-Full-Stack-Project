e49904abcd9c286631b75ed25f47cd991dde778c
Rayy-007 <minkhant88887@gmail.com>
2023-11-20 11:30:31 +0630

    Createe Navbar Dynamic Links

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,7 +1,13 @@
 import "./navbar.css";
 import { Logo } from "../ImageImport";
+import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
+  const activeStyle = {
+    fontWeight: 700,
+    color: "#3d87f5",
+  };
+
   return (
     <nav>
       <a href="/" className="logo-container">
@@ -11,16 +17,36 @@ const Navbar = () => {
       <div className="nav-links">
         <ul>
           <li>
-            <a href="/">Home</a>
+            <NavLink
+              style={({ isActive }) => (isActive ? activeStyle : null)}
+              to="/"
+            >
+              Home
+            </NavLink>
           </li>
           <li>
-            <a href="/about">About</a>
+            <NavLink
+              style={({ isActive }) => (isActive ? activeStyle : null)}
+              to="/about"
+            >
+              About
+            </NavLink>
           </li>
           <li>
-            <a href="/contact">Contact</a>
+            <NavLink
+              style={({ isActive }) => (isActive ? activeStyle : null)}
+              to="/contact"
+            >
+              Contact
+            </NavLink>
           </li>
           <li>
-            <a href="/terms">Terms</a>
+            <NavLink
+              style={({ isActive }) => (isActive ? activeStyle : null)}
+              to="/terms"
+            >
+              Terms
+            </NavLink>
           </li>
         </ul>
       </div>
