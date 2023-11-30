c46545e75cac144e98386589558c37b4980f544c
Rayy-007 <minkhant88887@gmail.com>
2023-11-30 11:05:39 +0630

    Restructure the Page import and Delete uncessary images

--- src/components/PageImport.js
+++ src/components/PageImport.js
@@ -5,5 +5,6 @@ import Footer from "./footer/Footer";
 import Contact from "./contact/Contact";
 import Register from "./register/Register";
 import Login from "./login/Login";
+import Terms from "./terms/Terms";
 
-export { Navbar, Home, Footer, About, Contact, Register, Login };
+export { Navbar, Home, Footer, About, Contact, Register, Login, Terms };
